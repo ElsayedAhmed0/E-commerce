eef97cdbac8f026f76b0e701ef3121982ad3cab9
elsayed khalil <e.khalil@evntoo.com>
2023-04-03 21:51:41 +0200

    add

--- src/app/product/components/secound-product-all-carts/secound-product-all-carts.component.ts
+++ src/app/product/components/secound-product-all-carts/secound-product-all-carts.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductService } from '../../service/product.service';
 @Component({
   selector: 'app-secound-product-all-carts',
   templateUrl: './secound-product-all-carts.component.html',
   styleUrls: ['./secound-product-all-carts.component.css']
 })
 export class SecoundProductAllCartsComponent implements OnInit {
-
-  constructor() { }
+  secProduct:any[]=[]
+  loading: boolean = false
+  constructor(private service: ProductService) { }
 
   ngOnInit(): void {
+    this. getTheSecProduct()
   }
+  getTheSecProduct(){
+    this.loading=true
+    this.service.getSecProduct().subscribe((res:any)=>{
+      this.loading=false
+      console.log(res);
 
+      this.secProduct=res
+    })
+  }
 }
